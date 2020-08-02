6352b3b0cc66df731dc9b9f18aa59029b5ee311f
Fernando <fernandofilho1310@gmail.com>
2020-08-02 15:51:10 -0300

    Agenda de Contatos

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:agenda_de_contatos/Ui/conctact_page.dart';
+import 'package:flutter/material.dart';
+
+import 'Ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
